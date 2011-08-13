3f53f3d47979b45f4f67fe0896b4a97a18b2c27d
Craig Bowles <cbowles@penelo.local>
2011-08-12 23:59:38 -0500

    Added some regexp utilities; began to fill out keywords and operators lists, along with regexps to match them

---
+++ ruby/lib/util/regexp_utils.rb
@@ -0,0 +1,39 @@
+
+module Cranberry
+  REGEXP_META_CHARS = ['/','^','[',']','.','$','{','}','*','(',')',
+                       '+','|','?','<','>']
+  REGEXP_META_CHARS_ESCAPED = ['\+'] #REGEXP_META_CHARS.collect{|c| '\\' + c}
+
+  def self.escape_regexp_meta_chars(s)
+    s = s.clone
+    REGEXP_META_CHARS.each do |c|
+      s.gsub!(c, "\\\\" + c)
+    end
+    s
+  end
+
+  def self.escape_regexp_meta_chars_all(strs)
+    strs.collect{|s| escape_regexp_meta_chars(s)}
+  end
+
+  def self.values_regexp(values)
+    values = escape_regexp_meta_chars_all(values)
+    Regexp.new("(#{values.join('|')})")
+  end
+
+  def self.end_values_regexp(values)
+    values = escape_regexp_meta_chars_all(values)
+    Regexp.new("(#{values.join('|')})$")
+  end
+
+  def self.full_values_regexp(values)
+    values = escape_regexp_meta_chars_all(values)
+    Regexp.new("^(#{values.join('|')})$")
+  end
+
+  def self.start_values_regexp(values)
+    values = escape_regexp_meta_chars_all(values)
+    Regexp.new("^(#{values.join('|')})")
+  end
+end
+
